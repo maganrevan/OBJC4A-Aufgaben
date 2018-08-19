27fbd11c625c0f3d72854990ebc83208ec294ead
kruschwitzm <magnus.kruschwitz@i12.de>
2018-08-19 22:25:33 +0200

    Initial Commit

---
+++ OBJC4A-Aufgaben/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  OBJC4A-Aufgaben
+//
+//  Created by Magnus Kruschwitz on 19.08.18.
+//  Copyright © 2018 Magnus Kruschwitz. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
