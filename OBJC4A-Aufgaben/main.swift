0723eb64f14823bf70ebbef866f6b54dea0ec8ee
kruschwitzm <magnus.kruschwitz@i12.de>
2018-09-05 15:50:54 +0200

    Add Code Task 2.2

--- OBJC4A-Aufgaben/main.swift
+++ OBJC4A-Aufgaben/main.swift
@@ -10,8 +10,21 @@
 
 import Foundation
 
+/* Aufgabe 2.2 Beginn */
+/*
+func eingabe(testArray : [Double]) {
+    for i in 0..<testArray.count {
+        print(testArray[i])
+    }
+}
+var zahlen = [1.2, 3.4, 5.3, 5.7, 9.01]
+eingabe(testArray : zahlen)
+*/
+/* Aufgabe 2.3 Ende */
+
 /* Aufgabe 1.3 Beginn */
 
+/*
 func wieOft (iCounterLoc : Int) -> Int{
     let iReturn = iCounterLoc + 1
     return iReturn
@@ -25,6 +38,7 @@ iCounterGlob = wieOft(iCounterLoc: iCounterGlob)
 
 
 print("Die Funktion wieOft() wurde insgesammt \(iCounterGlob) mal aufgeraufen!")
+ */
 
 
 /* Aufgabe 1.3 Ende */
